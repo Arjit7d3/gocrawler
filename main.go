d45b6ac5e6da5466658f79efffdea8017fdf235e
Arjit Rajvanshi <arjitsystem03@gmail.com>
2025-12-01 08:55:34 +0530

    Added tests for necessary code functions, connection to webpage has been established and helper functions have been designed

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(args) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	rawURL := args[0]
+	fmt.Printf("starting crawl of: %s...\n", rawURL)
+
+	htmlbody, err := getHTML(rawURL)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	fmt.Print(htmlbody)
 }
